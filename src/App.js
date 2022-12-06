da9900595beb00cb9aabbd5b61df7f8c4f17c234
lalithcodelover <lalith.vlr@gmail.com>
2022-12-06 23:34:26 +0530

    Editor added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import "./App.css";
 import "../node_modules/react-bootstrap/dist/react-bootstrap";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import Login from "./components/Login";
+import Login from "./pages/Login";
 import { Route, Switch } from "react-router-dom";
 import { Fragment } from "react";
-import Welcome from "./components/Welcome";
+import Welcome from "./pages/Welcome";
+import ComposeMail from "./pages/ComposeMail";
+import Mailbox from "./pages/Mailbox";
+import SentMail from "./pages/SentMail";
 
 function App() {
   return (
     <Fragment>
       <Switch>
-        <Route path="/login">
+        <Route path="/login" exact>
           <Login />
         </Route>
         <Route path="/welcome">
           <Welcome />
         </Route>
+        <Route path="/compose">
+          <ComposeMail />
+        </Route>
+        <Route path="/mailbox">
+          <Mailbox />
+        </Route>
+        <Route path="/sentmail">
+          <SentMail />
+        </Route>
       </Switch>
-      </Fragment>
+    </Fragment>
   );
 }
 
